01a5408f76bd306e7705d8201081f146047d9141
trungls1706 <trungls1706@gmail.com>
2023-05-01 21:43:57 +0700

    update

--- src/App.js
+++ src/App.js
@@ -2,10 +2,11 @@ import { LocomotiveScrollProvider } from "react-locomotive-scroll";
 import { ThemeProvider } from "styled-components";
 import GlobalStyle from "./assets/styles/globalStyles";
 import { dark } from "./assets/styles/themes";
-import {useRef} from 'react'
-import { useLocomotiveScroll } from 'react-locomotive-scroll'
+import { useRef } from "react";
+import { useLocomotiveScroll } from "react-locomotive-scroll";
 import Home from "./sections/home";
-import 'locomotive-scroll/dist/locomotive-scroll.esm'
+import "locomotive-scroll/dist/locomotive-scroll.esm";
+import { AnimatePresence } from "framer-motion";
 function App() {
   const containerRef = useRef(null);
 
@@ -27,9 +28,11 @@ function App() {
           }
           containerRef={containerRef}
         >
-          <main data-scroll-container ref={containerRef}>
-            <Home/>
-          </main>
+          <AnimatePresence>
+            <main data-scroll-container ref={containerRef}>
+              <Home />
+            </main>
+          </AnimatePresence>
         </LocomotiveScrollProvider>
       </ThemeProvider>
     </>
